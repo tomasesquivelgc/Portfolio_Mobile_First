ab8b8d3d9eaf4fb5b98592d0530ef4a88f3bfd8d
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-10 17:25:59 -0300

    pop-up menu add
    script.js create
    pop-up-menu HTML create
    CSS for pop-up-menu add
    JS functions to open and close menu create

---
+++ script.js
@@ -0,0 +1,11 @@
+function openMenu() {
+    var x = document.getElementById("pop-up-menu");
+
+      x.style.display = "block";
+    
+  }
+
+function closeMenu() {
+    var x = document.getElementById("pop-up-menu");
+    x.style.display = "none";
+}
