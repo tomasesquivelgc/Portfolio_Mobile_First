9f73e89ee57fecd3e4c37e34d9c618c0cfc93b76
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-11 19:23:06 -0300

    pop-up-window create
    js object project add
    yoga project add (as test)
    js function to create div add, append to button

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ closeButton.forEach((item) => {
     y.style.display = 'none';
   };
 });
+
+const projects = {
+  yoga : {
+    Id: "yoga-project",
+    title:  "Multi-Post Stories",
+    technology1: "CSS",
+    technology2: "HTML",
+    technology3: "Bootstrap",
+    technology4: "Ruby",
+    image: "./Normal_Button/yoga-img.jpgX",
+    paragraph: "A daily selection of privately personalized reads; no accounts or sign-ups required. This has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  }
+}
+
+function showNewSection(projectId) {
+  var container = document.getElementById(projects[projectId].Id);
+  var newSection = document.createElement("div");
+  newSection.id = "pop-up-window";
+  newSection.innerHTML = "<h3>" + projects[projectId].title +"<button id='close-menu-button' class='close-menu-button'><img src='./Normal_Button/Close_button.png' alt='Close menu'></button>" + "</h3>" +
+                          "<ul>" + "<li>" + "<p>" + projects[projectId].technology1 + "</p>"+ "</li>" + "<li>"+ "<p>" + projects[projectId].technology2 + "</p>" + "</li>" + "<li>"+ "<p>" + projects[projectId].technology3 + "</p>" + "</li>" + "<li>"+ "<p>" + projects[projectId].technology4 + "</p>" + "</li>" + "</ul>" +
+                          "<img src='" + projects[projectId].image + "' alt='" + projects[projectId].title + "' />" +
+                          "<p>" + projects[projectId].paragraph + "</p>" + 
+                          "<div><button>See Live</button>" + " " +"<button>See Source</button></div>";
+  container.appendChild(newSection);
+  newSection.style.display = "flex";
+}                
