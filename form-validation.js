bef84c9eaeef6b325d9736fe14cdf0880235296b
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-12 18:23:29 -0300

    js function change

--- form-validation.js
+++ form-validation.js
@@ -1,15 +1,20 @@
 function validateForm() {
   const nameInput = document.getElementById('full-name');
   const emailInput = document.getElementById('email');
-  const messageInput = document.getElementById('msg');
   const errorMessage = document.getElementById('error-message');
 
   // Reset the error message
-  errorMessage.innerHTML = ' ';
+  errorMessage.innerHTML = '';
 
   // Check if name is empty
   if (!nameInput.value) {
     errorMessage.innerHTML = 'Please enter your name.';
+    return false;
+  }
+
+  // Check if email is empty
+  if (!emailInput.value) {
+    errorMessage.innerHTML = 'Please enter your email address.';
     return false;
   }
 
@@ -19,14 +24,6 @@ function validateForm() {
     return false;
   }
 
-  // Check if message is empty
-  if (!messageInput.value) {
-    errorMessage.innerHTML = 'Please enter a message.';
-    return false;
-  }
-
   // Form is valid
   return true;
 }
-const submitButton = document.getElementById('submit');
-submitButton.onclick=validateForm();
