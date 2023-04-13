b34a5357b51485a4679d02e80f85ca1047087c85
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-13 20:03:17 -0300

    lolcalstorage function create

--- localStorage.js
+++ localStorage.js
@@ -1,6 +1,14 @@
+let formelements = document.getElementById('form-element');
+
 function setLocalStorageData(key, value) {
     localStorage.setItem(key, value);
-    console.log(`Stored ${value} with key ${key} in LocalStorage.`);
 };
-setLocalStorageData("name", "John Doe");
 
+form.oninput = function() {
+    let userName = formelements[0].innerHTML;
+    let userEmail = formelements[1].innerHTML;
+    let userText = formelements[2].innerHTML;
+    setLocalStorageData("name", userName);
+    console.log(localStorage.getItem('name'));
+    formelements[2].innerHTML = localStorage.getItem("name");
+};
