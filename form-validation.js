0b893cd0c74f600575b7506f66483971084f00ac
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-13 12:50:30 -0300

    form validation function change
    error message div change place in html and css add
    function now checks only the lowercasing of email

--- form-validation.js
+++ form-validation.js
@@ -1,29 +1,11 @@
-function validateForm() {
-  const nameInput = document.getElementById('full-name');
+const form = document.getElementById('form-element');
+function validateForm(event) {
   const emailInput = document.getElementById('email');
   const errorMessage = document.getElementById('error-message');
-
-  // Reset the error message
-  errorMessage.innerHTML = '';
-
-  // Check if name is empty
-  if (!nameInput.value) {
-    errorMessage.innerHTML = 'Please enter your name.';
-    return false;
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
+    event.preventDefault();
+    errorMessage.innerHTML = 'Please use only lowercase';
   }
-
-  // Check if email is empty
-  if (!emailInput.value) {
-    errorMessage.innerHTML = 'Please enter your email address.';
-    return false;
-  }
-
-  // Check if email is in lowercase
-  if (emailInput.value.toLowerCase() !== emailInput.value) {
-    errorMessage.innerHTML = 'Please enter your email address in lowercase.';
-    return false;
-  }
-
-  // Form is valid
-  return true;
 }
+
+form.addEventListener('submit', validateForm);
