368a2e0ab79854eb884136ba979067955bde2e20
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-12 22:11:43 -0300

    render function fix
    function now takes values from array inside object.
    image and paragraph+buttons padding add
    paragraph+button justify content space between

--- script.js
+++ script.js
@@ -15,31 +15,34 @@ closeButtons.forEach((item) => {
 const projects = {
   yoga: {
     title: 'Multi-Post Stories',
-    technologies: ["CSS", "HTML", "Bootstrap", "Ruby"],
+    technologies: ['CSS', 'HTML', 'Bootstrap', 'Ruby'],
     image: './Normal_Button/yoga-img.jpg',
     paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    link: "link_to_live_version.com",
   },
 
   artprinting: {
     title: 'profesional-art-printing-data',
-    technologies:["HTML", "Bootstrap", 'Ruby on Rails', "Ruby"],
+    technologies: ['HTML', 'Bootstrap', 'Ruby on Rails', 'Ruby'],
     image: './Normal_Button/desktop-grid3-background-A.png',
     paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    link: "link_to_live_version.com",
   },
 
   dataDash: {
     title: 'Data Dashboard Healthcare',
-    technologies:["HTML", "Bootstrap", "Ruby"],
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
     image: './Normal_Button/data-dashboard-hovered.png',
     paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    link: "link_to_live_version.com",
   },
 
   webPort: {
     title: 'Website Portfolio',
-    technologies:["HTML", "Bootstrap", "Ruby"],
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
     image: './Normal_Button/web-portfolio-background.png',
     paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-
+    link: "link_to_live_version.com",
   },
 };
 
@@ -47,8 +50,12 @@ function showNewSection(projectId) {
   const container = document.getElementById('yoga-project');
   const newSection = document.createElement('div');
   newSection.id = 'pop-up-window';
+  let liItems = `<li><p class='new-section-technology'>${projects[projectId].technologies[0]}</p></li><li><p class='new-section-technology'>${projects[projectId].technologies[1]}</p></li><li><p class='new-section-technology'>${projects[projectId].technologies[2]}</p></li>`;
+  if (projects[projectId].technologies.length > 3) {
+    liItems += `<li><p class='new-section-technology'>${projects[projectId].technologies[3]}</p></li>`;
+  }
   newSection.innerHTML = `<div class='pop-up-container'><h3 style='margin: 0;'>${projects[projectId].title}<button id='closePopUp' class='close-pop-up' onclick='closeNewSection()'><img src='./Normal_Button/Close_button.png' alt='Close menu'></button></h3>
-                          <ul><li><p class='new-section-technology'>${projects[projectId].technologies[0]}</p></li><li><p class='new-section-technology'>${projects[projectId].technologies[1]}</p></li><li><p class='new-section-technology'>${projects[projectId].technologies[2]}</p></li><li><p class='new-section-technology'>${projects[projectId].technologies[3]}</p></li></ul>
+                          <ul>${liItems}</ul>
                           <img src='${projects[projectId].image}' alt='${projects[projectId].title}' class='pop-up-image'/>
                           <div class='text-and-buttons'><p>${projects[projectId].paragraph}</p>
                           <div class=button-div><button class=see-project1>See Live<img src='./Normal_Button/seeLive-button.svg' alt='See live'></button><button class='see-project1'>See Source<img src='./Normal_Button/seeSource-button.svg' alt='See source'</button></div></div></div>';`;
