50ec98bc0b0c9c6355d102d467c6d52f27952fbe
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-14 12:40:09 -0300

    lynters fix

--- localStorage.js
+++ localStorage.js
@@ -1,21 +1,21 @@
-let formelement = document.getElementById('form-element');
-let userName = formelement[0];
-let userEmail = formelement[1];
-let userText = formelement[2];
+const formelement = document.getElementById('form-element');
+const userName = formelement[0];
+const userEmail = formelement[1];
+const userText = formelement[2];
 
-form.oninput = function() {
-    const data = {
-        saveName : userName.value,
-        saveEmail : userEmail.value,
-        saveText : userText.value,
-    };
-    localStorage.setItem("formdata", JSON.stringify(data))
+formelement.oninput = function saveData () {
+  const data = {
+    saveName: userName.value,
+    saveEmail: userEmail.value,
+    saveText: userText.value,
+  };
+  localStorage.setItem('formdata', JSON.stringify(data));
 };
 
 function loadData() {
-    const dataForm = JSON.parse(localStorage.getItem("formdata"));
-    userName.value = dataForm.saveName;
-    userEmail.value = dataForm.saveEmail;
-    userText.value = dataForm.saveText;
+  const dataForm = JSON.parse(localStorage.getItem('formdata'));
+  userName.value = dataForm.saveName;
+  userEmail.value = dataForm.saveEmail;
+  userText.value = dataForm.saveText;
 }
 loadData();
