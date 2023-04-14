317d11f8be264a468d2c0acbd46103b25e1d9dcd
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-14 12:24:06 -0300

    saveData function create

--- localStorage.js
+++ localStorage.js
@@ -1,14 +1,18 @@
-let formelements = document.getElementById('form-element');
-
-function setLocalStorageData(key, value) {
-    localStorage.setItem(key, value);
-};
+let formelement = document.getElementById('form-element');
+let userName = formelement[0];
+let userEmail = formelement[1];
+let userText = formelement[2];
 
 form.oninput = function() {
-    let userName = formelements[0].innerHTML;
-    let userEmail = formelements[1].innerHTML;
-    let userText = formelements[2].innerHTML;
-    setLocalStorageData("name", userName);
-    console.log(localStorage.getItem('name'));
-    formelements[2].innerHTML = localStorage.getItem("name");
+    const data = {
+        saveName : userName.value,
+        saveEmail : userEmail.value,
+        saveText : userText.value,
+    };
+    localStorage.setItem("formdata", JSON.stringify(data))
 };
+
+function loadData() {
+   // userName.value = "JSON.parse(localStorage.getItem(.saveName))";
+}
+loadData();
