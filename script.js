35632ff38b87e6dae5cafb34555ea27361e0f9e3
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-03-15 13:23:43 -0300

    Fill out project section (#21)
    * add new screenshots
    
    * add links to social media bar
    
    * add recipe project
    
    * add bookcar project
    
    * add space hub project
    
    * add ruby catalog
    
    * add last project and resume link
    
    * add logo face and fix footer links
    
    * fix linters

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ const projects = {
     technologies: ['CSS', 'HTML', 'ES6', 'Luxon'],
     image: './Normal_Button/awesome_books_2.png',
     paragraph: "This is a one page application using basic CSS classes to hide and show the section you want to see with the navBar. The 'add Book' input adds a class instance into our list and then renders it for a complete view of the library. The complete list is then saved to the local storage for everyday use. Try it out in the live demo!",
-    link: 'link_to_live_version.com',
-    codeLink: 'https://github.com/tomasesquivelgc/Games-Sales-Hunter',
+    link: 'https://tomasesquivelgc.github.io/Awesome-books/',
+    codeLink: 'https://github.com/tomasesquivelgc/Awesome-books',
   },
 
   artprinting: {
@@ -53,10 +53,56 @@ const projects = {
     title: 'Game Sales Hunter',
     technologies: ['Javascript', 'React', 'Redux'],
     image: './project_images/game_sales/game_sale_screenshot_3.png',
-    paragraph: 'Mobile App using CheapShark Api to find the best deals on games. The user can search for a specific game or browse through the deals list. By clickin, the user can also see the games details and a link to the official steam page',
+    paragraph: 'Mobile App using CheapShark Api to find the best deals on games. The user can search for a specific game or browse through the deals list. By clicking, the user can also see the games details and a link to the official steam page',
     link: 'https://monumental-raindrop-ef8680.netlify.app',
     codeLink: 'https://github.com/tomasesquivelgc/Games-Sales-Hunter',
   },
+
+  recipeApp: {
+    title: 'Recipe App',
+    technologies: ['Ruby', 'Rails', 'PostgreSQL'],
+    image: './project_images/recipe_app/recipe_app_screenshot_2.png',
+    paragraph: 'Web App that allows users to create, edit, and delete recipes. The user can also add ingredients and steps to the recipe, as well as deciding if their recipe is public or not.',
+    link: '',
+    codeLink: 'https://github.com/tomasesquivelgc/Recipe-app',
+  },
+
+  bookCar: {
+    title: 'Book a Car',
+    technologies: ['Ruby', 'Rails', 'PostgreSQL', 'React', 'Redux'],
+    image: './project_images/bookcar/book_car_screenshot_2.png',
+    paragraph: 'Full-stack app built with Ruby on Rails and React/Redux. The user can book a car for a specific date and time, as well as see the available cars and their details. The user can also see the available cities to rent. Admins can add new cars as well as delete them.',
+    link: 'https://fmanimashaun.github.io/book-car/',
+    codeLink: 'https://github.com/tomasesquivelgc/book-car',
+  },
+
+  spaceTravel: {
+    title: 'Space Travel',
+    technologies: ['Javascript', 'React', 'Redux'],
+    image: './project_images/space_travel/space_hub_screenshot_3.png',
+    paragraph: 'Web App that allows users to navigate a list of rockets or missions and book them. The user can also navigate to their booked items. This app was built using the SpaceX API.',
+    link: 'https://deploy-preview-36--guileless-halva-645039.netlify.app',
+    codeLink: 'https://github.com/tomasesquivelgc/Space-Travelers-Hub',
+  },
+
+  catalogOfThings: {
+    title: 'Catalog of Things',
+    technologies: ['Ruby', 'JSON', 'SQL'],
+    image: './project_images/ruby_catalog/Items_list.png',
+    paragraph: 'Ruby console app that allows users to create, read and delete an asort of games, books and albums. The user can also see the list of items and their details as well as a list of genres, authors and labels.',
+    link: '',
+    codeLink: 'https://github.com/tomasesquivelgc/My-Things-Catalog/tree/dev',
+  },
+
+  anemoia: {
+    title: 'Anemoia',
+    technologies: ['Javascript', 'HTML', 'CSS3'],
+    image: './project_images/anemoia/anemoia_screenshot_3.png',
+    paragraph: 'Landing page for a fictional event built with simple HTML, CSS and JavaScript. The page is fully responsive with a mobile-first approach. Sections are hidden and shown with a simple JavaScript function and created with DOM manipulation.',
+    link: 'https://precious-dolphin-41f59b.netlify.app',
+    codeLink: 'https://github.com/tomasesquivelgc/module1-capstone',
+  },
+
 };
 
 function showNewSection(projectId) {
@@ -71,7 +117,7 @@ function showNewSection(projectId) {
                           <ul>${liItems}</ul>
                           <img src='${projects[projectId].image}' alt='${projects[projectId].title}' class='pop-up-image'/>
                           <div class='text-and-buttons'><p>${projects[projectId].paragraph}</p>
-                          <div class='button-div'><button class='see-project1'><a href=${projects[projectId].link}>See Live</a><img src='./Normal_Button/seeLive-button.svg' alt='See live'></button><button class='see-project1'><a href=${projects[projectId].codeLink}>See Source</a><img src='./Normal_Button/seeSource-button.svg' alt='See source'</button></div></div></div>';`;
+                          <div class='button-div'><button class='see-project1'><a href=${projects[projectId].link} target="_blank">See Live</a><img src='./Normal_Button/seeLive-button.svg' alt='See live'></button><button class='see-project1'><a href=${projects[projectId].codeLink} target="_blank">See Source</a><img src='./Normal_Button/seeSource-button.svg' alt='See source'</button></div></div></div>';`;
   container.appendChild(newSection);
   newSection.style.display = 'flex';
 }
@@ -85,3 +131,8 @@ closePopUp.onclick = closeNewSection();
 
 const openPopUp = document.getElementById('popButton');
 openPopUp.onclick = showNewSection();
+
+// eslint-disable-next-line no-unused-vars
+function openResume() {
+  window.open('https://docs.google.com/document/d/e/2PACX-1vRgXEd3utL9Uql57taDOidPbsYLvzCnwtv_B22REQOEhlTgC6zVEXrgss-M-YwjkjGsK22qdDXvzRmh/pub', '_blank');
+}
